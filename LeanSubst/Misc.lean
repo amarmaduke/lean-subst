176b56b52ff040f88b6a849216745599c58fad2e
Sage Binder <sage.binder@pm.me>
2026-06-30 14:15:56 -0500

    many lemmas done

--- LeanSubst/Misc.lean
+++ LeanSubst/Misc.lean
@@ -250,9 +250,9 @@ theorem Subst.lift_of_succ [RenMap S S] [RenMapCompose S S] {k} {σ : Subst S} :
   funext n ; induction n
   case zero => simp
   case succ n' _  =>
-    simp ; split
-    · simp
-    · simp [rmap] ; split <;> grind [Ren.add, Ren.succ, Ren.compose]
+    simp
+    split <;> simp [rmap]
+    split <;> grind [Ren.add, Ren.succ, Ren.compose]
 
 theorem Subst.lift_of_succ_rev [RenMap S S] [RenMapCompose S S] {k} {σ : Subst S} : σ.lift (1 + k) = σ.lift.lift k := by
   rw [Nat.add_comm, lift_of_succ]
@@ -273,49 +273,65 @@ theorem Subst.lift_of_add [RenMap S S] [RenMapId S S] [RenMapCompose S S] {a b} 
   induction a generalizing σ <;> grind [lift_of_succ_rev]
 
 @[simp]
-theorem Subst.ren_to_hcompose [SubstMap S T] {r : Ren S} {σ : Subst T} : r.to ◾ σ = r.to := sorry
+theorem Subst.ren_to_hcompose [SubstMap S T] {r : Ren S} {σ : Subst T} : r.to ◾ σ = r.to := by simp [hcompose, Ren.to]
 
 @[simp]
-theorem Subst.ren_to_hcompose_ren [RenMap S T] {r : Ren S} {k : Ren T} : r.to ◾ k = r.to := sorry
+theorem Subst.ren_to_hcompose_ren [RenMap S T] {r : Ren S} {k : Ren T} : r.to ◾ k = r.to := by simp [hcompose_ren, Ren.to]
 
 @[simp]
-theorem Subst.to_append {ℓ : List Nat} {r : Ren T} : (ℓ ++ r).to = ℓ ++ r.to := sorry
+theorem Subst.to_append {ℓ : List Nat} {r : Ren T} : (ℓ ++ r).to = ℓ ++ r.to := by
+  induction ℓ <;> simp_all [HAppend.hAppend, Ren.append, append_ren]
 
 @[simp, grind =]
-theorem Subst.ren_rewrite1 [RenMap T T] {r : Ren T} : id T ∘ r = r.to := sorry
+theorem Subst.ren_rewrite1 [RenMap T T] {r : Ren T} : id T ∘ r = r.to := by simp [Ren.to, compose_ren_right]
 
 @[simp, grind =]
-theorem Subst.ren_rewrite1_left {r : Ren T} : r ∘ id T = r.to := sorry
+theorem Subst.ren_rewrite1_left {r : Ren T} : r ∘ id T = r.to := by simp [Ren.to, compose_ren_left]
 
 theorem Subst.compose_lift_append_indirect {k}
   [RenMap S S] [RenMapId S S] [RenMapCompose S S]
   [SubstMap S S] [SubstMapId S S]
   [SubstMapRenComposeLeft S S] [SubstMapCompose S S]
   {ℓ1 ℓ2 : List (Action S)} (h : k = ℓ2.length)
-  : (ℓ1 ++ Subst.id S).lift k ∘ (ℓ2 ++ Subst.id S) = (ℓ2 ++ ℓ1) ++ Subst.id S
-:= by
-  sorry
+  : (ℓ1 ++ Subst.id S).lift k ∘ (ℓ2 ++ Subst.id S) = (ℓ2 ++ ℓ1) ++ Subst.id S := by
+  induction ℓ1
+  case nil => simp
+  case cons hd tl _ => sorry
 
 theorem Subst.compose_left_cons_lift_indirect {k}
-  [RenMap T T] [SubstMap T T] {ℓ : List $ Action T} {σ τ : Subst T} {h : k = ℓ.length}
-  : σ.lift k ∘ (ℓ ++ τ) = ℓ ++ (σ ∘ τ)
-:= sorry
+  [RenMap T T] [SubstMap T T] [RenMapId T T] {ℓ : List $ Action T} {σ τ : Subst T} {h : k = ℓ.length}
+  : σ.lift k ∘ (ℓ ++ τ) = ℓ ++ (σ ∘ τ) := by
+  induction ℓ generalizing k
+  case nil =>
+    have kz : k = 0 := by grind
+    simp [kz, lift, compose]
+  case cons x xs ih =>
+    have kpred : k.pred = xs.length := by simp_all
+    have ih' := @ih k.pred kpred
+    simp
+    simp [cons, compose]
+    funext n
+    simp [smap]
+    sorry
 
 @[simp]
 theorem Subst.List.smap_append [SubstMap S T] {a b : List S} {σ : Subst T}
-  : (a ++ b)[σ] = a[σ] ++ b[σ]
-:= sorry
+  : (a ++ b)[σ] = a[σ] ++ b[σ] := by induction a <;> grind
 
 @[simp]
-theorem Subst.List.rmap_reverse [RenMap S T] {ℓ : List S} {r : Ren T} : ℓ.reverse⟨r⟩ = ℓ⟨r⟩.reverse := sorry
+theorem Subst.List.rmap_reverse [RenMap S T] {ℓ : List S} {r : Ren T} : ℓ.reverse⟨r⟩ = ℓ⟨r⟩.reverse := by
+  induction ℓ <;> simp ; grind
 
 @[simp]
-theorem Subst.List.smap_reverse [SubstMap S T] {ℓ : List S} {σ : Subst T} : ℓ.reverse[σ] = ℓ[σ].reverse := sorry
+theorem Subst.List.smap_reverse [SubstMap S T] {ℓ : List S} {σ : Subst T} : ℓ.reverse[σ] = ℓ[σ].reverse := by
+  induction ℓ <;> simp ; grind
 
 @[simp]
-theorem Subst.List.rmap_map_su [RenMap T T] {ℓ : List T} {r : Ren T} : (List.map su ℓ)⟨r⟩ = List.map su ℓ⟨r⟩ := sorry
+theorem Subst.List.rmap_map_su [RenMap T T] {ℓ : List T} {r : Ren T} : (List.map su ℓ)⟨r⟩ = List.map su ℓ⟨r⟩ := by
+  induction ℓ <;> simp ; grind
 
 @[simp]
-theorem Subst.List.smap_map_su [SubstMap T T] {ℓ : List T} {σ : Subst T} : (List.map su ℓ)[σ] = List.map su ℓ[σ] := sorry
+theorem Subst.List.smap_map_su [SubstMap T T] {ℓ : List T} {σ : Subst T} : (List.map su ℓ)[σ] = List.map su ℓ[σ] := by
+  induction ℓ <;> simp ; grind
 
 end LeanSubst
