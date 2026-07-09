236f93c2f0aa474b66b2f9d04a127b75db74985e
Sage Binder <sage.binder@pm.me>
2026-07-08 21:23:56 -0500

    parametrized attributes

--- LeanSubst/Automation/Attributes.lean
+++ LeanSubst/Automation/Attributes.lean
@@ -1,6 +1,22 @@
 import Batteries.Lean.TagAttribute
+import Lean.Parser.Term
 
 open Lean
 
 initialize leanSubstVar : TagAttribute ← registerTagAttribute `leansubst_var "Indicates that a constructor is a variable constructor."
 initialize leanSubstBinder : TagAttribute ← registerTagAttribute `leansubst_binder "Indicates that a constructor is a binder constructor."
+
+#check Lean.Parser.Command.classAbbrev
+#check Lean.Parser.Term.attrInstance
+#check Attr.coe
+
+initialize leanSubstBinder' : ParametricAttribute Nat ← registerParametricAttribute {
+  name := `leansubst_binder',
+  descr := "Blah",
+  getParam := fun
+    | name, stx@`(Lean.Parser.Attr.simple| $_ $n:num) => do
+      pure n.getNat
+    | name, stx@`(Lean.Parser.Term.attrInstance| $_) => do
+      dbg_trace s!"aw fuck {name}, {stx}"
+      pure 0
+}
--- LeanSubst/Automation/Basic.lean
+++ LeanSubst/Automation/Basic.lean
@@ -4,126 +4,6 @@ import LeanSubst.Automation.Attributes
 import Qq
 import Aesop
 
-namespace Examples.LambdaCalc
-  open LeanSubst
-
-  inductive Term where
-  | var : Nat -> Term
-  | app : Term -> Term -> Term
-  | lam : Term -> Term
-
-  prefix:100 ":λ " => Term.lam
-  infixl:65 " :@ " => Term.app
-
-  @[coe]
-  def Term.from_action : Action Term -> Term
-  | re y => var y
-  | su t => t
-
-  @[simp, grind =]
-  theorem Term.from_action_id {n} : from_action (+0σ.act n) = var n := by
-    simp [from_action]
-
-  @[simp, grind =]
-  theorem Term.from_action_succ {n} : from_action (+1σ.act n) = var (n + 1) := by
-    simp [from_action]
-
-  @[simp, grind =]
-  theorem Term.from_acton_re {n} : from_action (re n) = var n := by simp [from_action]
-
-  @[simp, grind =]
-  theorem Term.from_action_su {t} : from_action (su t) = t := by simp [from_action]
-
-  instance instCoe_SubstActionTerm_Term : Coe (Action Term) Term where
-    coe := Term.from_action
-
-  @[simp]
-  def rmap (r : Ren Term) : Term -> Term
-  | .var x => .var (r.act x)
-  | t1 :@ t2 => rmap r t1 :@ rmap r t2
-  | :λ t => :λ rmap r.lift t
-
-  def rmap' (r : Ren Term) : Term → Term
-  | Term.var x => Term.var (r.act x)
-  | Term.app t1 t2 => Term.app (rmap r t1) (rmap r t2)
-  | Term.lam t => Term.lam (rmap r.lift t)
-
-  instance : RenMap Term Term where
-    rmap := rmap
-
-  @[simp, grind =]
-  theorem ren_var {x} {r : Ren Term} : (Term.var x)⟨r⟩ = .var (r.act x) := by
-    simp [RenMap.rmap]
-
-  @[simp, grind =]
-  theorem ren_app {t1 t2} {r : Ren Term} : (t1 :@ t2)⟨r⟩ = t1⟨r⟩ :@ t2⟨r⟩ := by
-    simp [RenMap.rmap]
-
-  @[simp, grind =]
-  theorem ren_lam {t} {r : Ren Term} : (:λ t)⟨r⟩ = :λ t⟨r.lift⟩ := by
-    simp [RenMap.rmap]
-
-  instance : RenMapId Term Term where
-    apply_id := by subst_solve_id
-
-  instance : RenMapCompose Term Term where
-    apply_compose := by subst_solve_compose
-
-  @[simp]
-  def smap (σ : Subst Term) : Term -> Term
-  | .var x => σ.act x
-  | t1 :@ t2 => smap σ t1 :@ smap σ t2
-  | :λ t => :λ smap σ.lift t
-
-  instance SubstMap_Term : SubstMap Term Term where
-    smap := smap
-
-  @[simp, grind =]
-  theorem subst_var {x} {σ : Subst Term} : (Term.var x)[σ] = σ.act x := by
-    simp [SubstMap.smap]
-
-  @[simp, grind =]
-  theorem subst_app {t1 t2} {σ : Subst Term} : (t1 :@ t2)[σ] = t1[σ] :@ t2[σ] := by
-    simp [SubstMap.smap]
-
-  @[simp, grind =]
-  theorem subst_lam {t} {σ : Subst Term} : (:λ t)[σ] = :λ t[σ.lift] := by
-    simp [SubstMap.smap]
-
-  @[simp]
-  theorem Term.from_action_compose {x : Nat} {σ τ : Subst Term}
-    : (from_action (Subst.act σ x))[τ] = from_action ((σ ∘ τ).act x)
-  := by
-    simp [from_action, Subst.compose]
-    generalize zdef : σ.act x = z
-    cases z <;> simp [from_action]
-
-  @[simp]
-  theorem Term.from_action_compose_ren {x : Nat} {σ : Subst Term} {r : Ren Term}
-    : (from_action (σ.act x))⟨r⟩ = from_action ((σ ∘ r).act x)
-  := by
-    simp [Term.from_action]
-    generalize zdef : σ.act x = z
-    cases z <;> simp
-
-  instance : SubstMapId Term Term where
-    apply_id := by subst_solve_id
-
-  instance : SubstMapStable Term Term where
-    apply_stable := by subst_solve_stable
-
-  instance : SubstMapRenComposeLeft Term Term where
-    apply_ren_compose_left := by subst_solve_compose
-
-  instance : SubstMapRenComposeRight Term Term where
-    apply_ren_compose_right := by subst_solve_compose
-
-  instance : SubstMapCompose Term Term where
-    apply_compose := by subst_solve_compose
-
-end Examples.LambdaCalc
-
-
 namespace Examples.LambdaCalcAutomation
   open Lean Elab Tactic Meta LeanSubst Command Aesop
 
@@ -138,6 +18,7 @@ namespace Examples.LambdaCalcAutomation
 
   attribute [leansubst_var] Term.var
   attribute [leansubst_binder] Term.lam
+  attribute [leansubst_binder' 2] Term.lam
 
   def getConstructors (typeName : Name) : MetaM (List Name) := do
     let env ← getEnv
@@ -157,6 +38,11 @@ namespace Examples.LambdaCalcAutomation
     let env ← getEnv
     pure $ leanSubstBinder.hasTag env ctor
 
+  def isBinder' (ctor : Name) : MetaM (Option Nat) := do
+    let env ← getEnv
+    let blah := leanSubstBinder'.getParam? env ctor
+    pure blah
+
   def getBinderCtors (type : Name) : MetaM (List Name) := do
     let ctors ← getConstructors type
     List.filterM (isBinder) ctors
@@ -171,14 +57,13 @@ namespace Examples.LambdaCalcAutomation
     for ctor in ctors do
       let isVar ← isVar ctor
       let isBinder ← isBinder ctor
+      let isBinder' ← isBinder' ctor
       if isVar then
         IO.println s!"{ctor} is a var"
-      else if isBinder then
-        IO.println s!"{ctor} is a binder"
+      else if let some i := isBinder' then
+        IO.println s!"{ctor} is a binder with param {i}"
       else
         IO.println s!"{ctor} is neither a var nor a binder"
-
-    _ := lctx.addDecl
 
   def mapFst {α β} (ℓ : List (α × β)) := ℓ.map Prod.fst
 
